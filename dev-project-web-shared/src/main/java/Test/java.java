7f3b15b236dcb8db747eb3d602b419fe62130e38
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2014-04-03 01:34:14 +0530

    Immediate Child Project build with maven publish done. need to check with multi level child project and publish docs..

---
+++ dev-project-web-shared/src/main/java/Test/java.java
@@ -0,0 +1,10 @@
+package Test;
+
+/**
+ * Created by suresh on 3/30/2014.
+ */
+public class java {
+    public static void main(String[] args){
+        System.out.print("Oh My God....!!!!");
+    }
+}
